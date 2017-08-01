f742aa22787f79602cac498efa2b01d35bdd163f
kkxxll <550893685@qq.com>
2017-08-02 00:21:44 +0800

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/* 
+* @Author: kxiner
+* @Date:   2017-08-01 10:31:48
+* @Last Modified by:   kxiner
+* @Last Modified time: 2017-08-01 10:32:18
+*/
+
+'use strict';
---
+++ src/js/vendor/jquery.js
@@ -0,0 +1,8 @@
+/* 
+* @Author: kxiner
+* @Date:   2017-08-01 10:32:07
+* @Last Modified by:   kxiner
+* @Last Modified time: 2017-08-01 10:32:16
+*/
+
+'use strict';
